43a19a89d759a66781ea2f52d117d961060d040f
drfdev <drafff@gmail.com>
2020-07-18 21:49:07 +0300

    fix vector

--- src/main/kotlin/dev/drf/midas/glove/core/entity/Vector3d.kt
+++ src/main/kotlin/dev/drf/midas/glove/core/entity/Vector3d.kt
@@ -1,14 +1,12 @@
 package dev.drf.midas.glove.core.entity
 
-import dev.drf.midas.glove.common.abs
-
 class Vector3d(startPoint: Point3d, directionPoint: Point3d) {
     private val point: Point3d
 
     init {
-        val x = abs(directionPoint.x - startPoint.x)
-        val y = abs(directionPoint.y - startPoint.y)
-        val z = abs(directionPoint.z - startPoint.z)
+        val x = directionPoint.x - startPoint.x
+        val y = directionPoint.y - startPoint.y
+        val z = directionPoint.z - startPoint.z
 
         point = Point3d(x, y, z)
     }
