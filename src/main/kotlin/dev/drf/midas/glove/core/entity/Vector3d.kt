96a56a8a6d228ffb5f15609b27296856fc9e64e2
drfdev <drafff@gmail.com>
2020-07-18 21:50:19 +0300

    small fix in vector naming

--- src/main/kotlin/dev/drf/midas/glove/core/entity/Vector3d.kt
+++ src/main/kotlin/dev/drf/midas/glove/core/entity/Vector3d.kt
@@ -1,12 +1,12 @@
 package dev.drf.midas.glove.core.entity
 
-class Vector3d(startPoint: Point3d, directionPoint: Point3d) {
+class Vector3d(startPoint: Point3d, destinationPoint: Point3d) {
     private val point: Point3d
 
     init {
-        val x = directionPoint.x - startPoint.x
-        val y = directionPoint.y - startPoint.y
-        val z = directionPoint.z - startPoint.z
+        val x = destinationPoint.x - startPoint.x
+        val y = destinationPoint.y - startPoint.y
+        val z = destinationPoint.z - startPoint.z
 
         point = Point3d(x, y, z)
     }
