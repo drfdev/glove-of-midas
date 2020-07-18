facf79671690dbeb02e1e76cd0b7f871469a0d4f
drfdev <drafff@gmail.com>
2020-07-18 18:00:08 +0300

    add junit5

---
+++ src/test/kotlin/dev/drf/midas/glove/MainTest.kt
@@ -0,0 +1,11 @@
+package dev.drf.midas.glove
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class MainTest {
+    @Test
+    fun mainTest() {
+        Assertions.assertEquals(1, 1);
+    }
+}
