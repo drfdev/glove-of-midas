af5f87c35c3cd15ab04f24d6f76d56c1f434dc61
drfdev <drafff@gmail.com>
2020-08-11 22:58:03 +0300

    add triangulation util

---
+++ glove-of-midas-core/src/main/kotlin/dev/drf/midas/glove/common/math/shape/TriangulationUtil.kt
@@ -0,0 +1,10 @@
+package dev.drf.midas.glove.common.math.shape
+
+import dev.drf.midas.glove.core.entity.basic.Point3d
+
+fun triangulation(basePoint1: Point3d, basePoint2: Point3d, basePoint3: Point3d,
+                  length1: Double, length2: Double, length3: Double): Point3d {
+    // TODO
+
+    return Point3d.ZERO_POINT
+}
