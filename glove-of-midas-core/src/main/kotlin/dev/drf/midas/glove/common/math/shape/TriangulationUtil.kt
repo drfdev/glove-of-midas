a049e969bb9a4601eda867eb0e79ee255e173473
drfdev <drafff@gmail.com>
2020-09-10 23:00:28 +0300

    triangulation implemented

--- glove-of-midas-core/src/main/kotlin/dev/drf/midas/glove/common/math/shape/TriangulationUtil.kt
+++ glove-of-midas-core/src/main/kotlin/dev/drf/midas/glove/common/math/shape/TriangulationUtil.kt
@@ -1,6 +1,7 @@
 package dev.drf.midas.glove.common.math.shape
 
 import dev.drf.midas.glove.core.entity.basic.Point3d
+import kotlin.math.pow
 
 /**
  * Result: the point of intersection of the three spheres centered at the base points
@@ -34,7 +35,59 @@ import dev.drf.midas.glove.core.entity.basic.Point3d
  */
 fun triangulation(basePoint1: Point3d, basePoint2: Point3d, basePoint3: Point3d,
                   length1: Double, length2: Double, length3: Double): Point3d {
-    // TODO
+    val a11 = basePoint1.x.value - basePoint2.x.value
+    val a12 = basePoint1.y.value - basePoint2.y.value
+    val a13 = basePoint1.z.value - basePoint2.z.value
 
-    return Point3d.ZERO_POINT
+    val a21 = basePoint1.x.value - basePoint3.x.value
+    val a22 = basePoint1.y.value - basePoint3.y.value
+    val a23 = basePoint1.z.value - basePoint3.z.value
+
+    val a31 = basePoint2.x.value - basePoint3.x.value
+    val a32 = basePoint2.y.value - basePoint3.y.value
+    val a33 = basePoint2.z.value - basePoint3.z.value
+
+    val t12 = calculateT(basePoint1, basePoint2, length1, length2)
+    val t13 = calculateT(basePoint1, basePoint3, length1, length3)
+    val t23 = calculateT(basePoint2, basePoint3, length2, length3)
+
+    val det = Matrix3x3(a11, a12, a13,
+                        a21, a22, a23,
+                        a31, a32, a33)
+            .determinant()
+
+    val det1 = Matrix3x3(t12, a12, a13,
+                         t13, a22, a23,
+                         t23, a32, a33)
+            .determinant()
+    val det2 = Matrix3x3(a11, t12, a13,
+                         a21, t13, a23,
+                         a31, t23, a33)
+            .determinant()
+    val det3 = Matrix3x3(a11, a12, t12,
+                         a21, a22, t13,
+                         a31, a32, t23)
+            .determinant()
+
+    val x1 = det1 / det
+    val x2 = det2 / det
+    val x3 = det3 / det
+
+    return Point3d(x1, x2, x3)
+}
+
+/**
+ * b^2 = x0^2 + y0^2 + z0^2
+ * T(1,2) = (R2^2 - R1^2 + b1^2 - b2^2) / 2
+ */
+private fun calculateT(point1: Point3d, point2: Point3d,
+                       length1: Double, length2: Double): Double {
+    val b1 = calculateB(point1)
+    val b2 = calculateB(point2)
+
+    return ( length2.pow(2) - length1.pow(2) + b1.pow(2) - b2.pow(2) ) / 2
+}
+
+private fun calculateB(point: Point3d): Double {
+    return point.x.value.pow(2) + point.y.value.pow(2) + point.z.value.pow(2)
 }
